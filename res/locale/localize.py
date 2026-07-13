dc6d45eab31feeaf34e7affab36283f860dbbeeb
ZZR <zzr4028@163.com>
2026-07-13 18:18:59 +0800

    add: localization support

---
+++ res/locale/localize.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import pathlib
+import inspect
+import subprocess
+
+if __name__ != '__main__':
+    print('This module is not designed to be imported', file=sys.stderr)
+
+
+if len(sys.argv) < 2:
+    print('Usage: python localize.py <command>\n'
+          '  extract\tExtract entries to template and update\n'
+          '  compile\tCompile language resource\n',
+          file=sys.stderr)
+    sys.exit(1)
+
+
+command = sys.argv[1]
+BASE_DIR = pathlib.Path(os.path.dirname(inspect.getfile(inspect.currentframe()))).parent.parent
+cwd = os.getcwd()
+os.chdir(BASE_DIR)
+
+match command:
+    case 'extract':
+        subprocess.run(['pybabel', 'extract', '-o', 'res/locale/app.pot', '.'])
+        subprocess.run(['pybabel', 'update', '-i', 'res/locale/app.pot', '-d', 'res/locale'])
+    case 'compile':
+        subprocess.run(['pybabel', 'compile', '-d', 'res/locale'])
+
+os.chdir(cwd)
